dd7ba47c2e3730e4cc46c27073635241f7871be8
MariaSoleim <maria@soleim.com>
2020-01-16 22:13:29 +0100

    Creates a graph of sMAPE for all the resolutions

---
+++ analysis/sMAPE-graph.py
@@ -0,0 +1,17 @@
+from helper import *
+from numpy import *
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+
+for resolution in resolutions:
+    path = "../reproduced-results/analysis-results/" + resolution + "/average_sMAPE.csv"
+    if Path(path).is_file():
+        with open(path) as file:
+            coefficients = [float(i) for i in file.readline().split(",")]
+            plt.plot(range(1, len(coefficients)+1), coefficients, label=resolution)
+plt.xlabel("Timestep after last observed value")
+plt.ylabel("Average sMAPE")
+plt.legend(loc='best')
+plt.savefig("../reproduced-results/analysis-results/sMAPE.png")
+plt.show()
