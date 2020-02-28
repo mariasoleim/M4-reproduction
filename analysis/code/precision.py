bfa9392f110475dd2ea3d37e4899b4088c405d24
Maria Soleim <masolei@stud.ntnu.no>
2020-02-28 13:21:10 +0100

    Changed get_average_sMAPE() to write a file instead of returning the result

--- analysis/code/precision.py
+++ analysis/code/precision.py
@@ -7,9 +7,9 @@ import numpy as np
 def sMAPE(value_1, value_2):
     """
     Calculate the sMAPE between two single values.
-    :param value_1: float
-    :param value_2: float
-    :return: float
+    :param value_1: Float
+    :param value_2: Float
+    :return: Float
     """
     return 200.0 * abs((value_2 - value_1)) / (abs(value_2) + abs(value_1))
 
@@ -18,9 +18,9 @@ def compare_results(file_1, file_2, output_path):
     """
     Takes in two files. Each file has a forecast for all the time series in the M4 competition. Writes a new file that
     contains the sMAPES between all the forecasted values. The files are expected to have a header line.
-    :param file_1: path to file 1
-    :param file_2: path to file 2
-    :return: nothing. Writes a new file with sMAPES
+    :param file_1: String. Path to file 1
+    :param file_2: String. Path to file 2
+    :return: Nothing. Writes a new file with sMAPES
     """
 
     file_1 = open(file_1).read().split("\n")
@@ -53,12 +53,13 @@ def compare_results(file_1, file_2, output_path):
         writer.writerow(sMAPEs)
 
 
-def get_average_sMAPE(path):
+def get_average_sMAPE(path, output_path):
     """
     Given the path to a file with one sMAPE value for each forecasted value for each time series, like the one
     generated in compare_results. Gives the average of all those values.
-    :param path: string, path to file
-    :return:
+    :param path: String. Path to file
+    :param output_path: String. Path to file in which to write the result.
+    :return: Nothing
     """
     reader = csv.reader(open(path))
 
@@ -78,7 +79,10 @@ def get_average_sMAPE(path):
             number_of_values += 1
 
     average_sMAPE = sum / number_of_values
-    return average_sMAPE
+
+    output_file = open(output_path)
+    output_file.write("Average sMAPE: ")
+    output_file.write(str(average_sMAPE))
 
 
 def get_sMAPE_for_each_timestep(path, output_path):
@@ -86,7 +90,7 @@ def get_sMAPE_for_each_timestep(path, output_path):
     For each timestep ahead in time, calculate the average sMAPE for this timestep.
     Since time series with different resolutions have different length of the forecasting horizon we will sort on
     resolution.
-    :param path: string. Path to a file with resolution for each timestep on each series
+    :param path: String. Path to a file with resolution for each timestep on each series
     :return: Writes a new file with average sMAPE for each series on each timestep
     """
     reader = csv.reader(open(path))
@@ -136,8 +140,9 @@ def resolution_timestep_sMAPE_graph(path, output_path):
     """
     Given a path to a file with average sMAPE for each resolution and each timestep (like the one made by
     get_sMAPE_for_each_timestep), creates a graph for this.
-    :param path:
-    :return:
+    :param path: String. Path to a file with average sMAPE for each resolution and each timestep (like the one made by
+    get_sMAPE_for_each_timestep)
+    :return: Nothing. Writes a file to output_path.
     """
     reader = csv.reader(open(path))
 
--- analysis/code/run.py
+++ analysis/code/run.py
@@ -4,7 +4,8 @@ for rerun in range(1, 6):
     rerun = str(rerun)
     compare_results("../../forecasts/118/rerun-" + rerun + "/forecast.csv", "../../data/test/all.csv",
                     "../results/118/rerun-" + rerun + "/precision.csv")
-    print(get_average_sMAPE("../results/118/rerun-" + rerun + "/precision.csv"))
+    get_average_sMAPE("../results/118/rerun-" + rerun + "/precision.csv",
+                      "../results/118/rerun-" + rerun + "/average_sMAPE.txt")
     get_sMAPE_for_each_timestep("../results/118/rerun-" + rerun + "/precision.csv", "../results/118/rerun-" +
                                 rerun + "/precision-resolution-timestep.csv")
     resolution_timestep_sMAPE_graph("../results/118/rerun-" + rerun + "/precision-resolution-timestep.csv",
@@ -12,3 +13,4 @@ for rerun in range(1, 6):
 
 rerun_sMAPEs = ["../results/118/rerun-" + str(rerun) + "/precision.csv" for rerun in range(1, 6)]
 get_average_sMAPEs_for_all_reruns("../results/118/precision.csv", *rerun_sMAPEs)
+get_average_sMAPE("../results/118/precision.csv", "../results/118/rerun-" + rerun + "/average_sMAPE.txt")
