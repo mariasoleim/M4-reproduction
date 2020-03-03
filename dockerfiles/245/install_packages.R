1ad71c1248b195f17797e208764ac3636d34812f
Maria Soleim <masolei@stud.ntnu.no>
2020-03-03 13:44:26 +0100

    Dockerfile for 245

---
+++ dockerfiles/245/install_packages.R
@@ -0,0 +1,11 @@
+install.packages("devtools")
+
+devtools::install_github("robjhyndman/M4metalearning")
+
+devtools::install_github("pmontman/tsfeatures")
+
+devtools::install_github("pmontman/customxgboost")
+
+install.packages("https://github.com/carlanetto/M4comp2018/releases/download/0.2.0/M4comp2018_0.2.0.tar.gz", repos=NULL)
+
+
