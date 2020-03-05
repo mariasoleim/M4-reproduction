21c3fcd357eb50c5310156ece0f6f1a9fd51db21
Maria Soleim <masolei@stud.ntnu.no>
2020-03-05 14:42:29 +0100

    Added dockerfile for method 69

---
+++ dockerfiles/069/install_packages.R
@@ -0,0 +1,7 @@
+install.packages("snow")
+install.packages("https://github.com/carlanetto/M4comp2018/releases/download/0.2.0/M4comp2018_0.2.0.tar.gz", repos=NULL)
+install.packages("forecast")
+install.packages("forecTheta")
+install.packages("dplyr")
+install.packages("Mcomp")
+
